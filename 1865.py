000c2f4963720ffda7d899a2dc17d3cab03c8413
Haeyong Hwang <41499598+haeyong27@users.noreply.github.com>
2019-03-28 15:20:41 +0900

    메모이제이션
    메모이제이션

--- 1865.py
+++ 1865.py
@@ -1,35 +1,36 @@
-n = 3
-s = '''13 1 50
-12 70 90
-25 60 100'''
-mapp = []
-s = s.split('\n')
-for i in s:
-    mapp.append(i.split(' '))
+T = int(input())
+for testcase in range(T):
 
-check = [0 for _ in range(n)]
+    n = int(input())
+    ans = 1
+    max = 0
+    check = [0 for _ in range(n)]
 
-for i in mapp:
-    print(i)
-ans = 1
-max = 0
-
-def dfs(a):
-    global ans
-    global max
-    if a == n:
-        if max < ans:
-            max = ans
-        return
-
+    mapp = []
     for i in range(n):
-        if check[i] == 1:
-            continue
-        check[i] = 1
-        ans = ans * int(mapp[a][i]) * .01
-        dfs(a+1)
-        ans = ans / int(mapp[a][i]) * 100
-        check[i] = 0
+        mapp.append(input().split(' '))
+    
+    def dfs(a, ans):
+        global max
+        if a == n:
+            if max < ans:
+                max = ans
+            return
+        if max >= ans:
+            return
+        for i in range(n):
+            if check[i] == 1:
+                continue
 
-dfs(0)
-print(max*100)
+            if int(mapp[a][i]) == 0:
+                continue
+
+
+
+            check[i] = 1
+            dfs(a+1, ans*int(mapp[a][i])*0.01)
+            check[i] = 0
+
+    dfs(0, 1)
+    print('#{} {:.6f}'.format(testcase+1, max * 100))
+
